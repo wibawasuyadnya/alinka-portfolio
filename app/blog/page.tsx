303080fc6a67cb5a81a1436eca6e0039aa21f917
wibawa suyadnya <wibawasuyadnya@wibawas-MacBook-Pro.local>
2024-09-23 19:49:45 +0800

    latest blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,25 @@
-"use client";
+import { Metadata } from "next";
 import Layout from "@/components/Layout";
 import Section from "@/components/Page-components/blog/Section";
 
+export const metadata: Metadata = {
+  title: {
+    default: "My Blog",
+    template: "%s - My Blog",
+  },
+  description: "Come and read my awesome articles",
+  twitter: {
+    card: "summary_large_image",
+  },
+  alternates: {
+    canonical: "./",
+  },
+};
+
 export default function Blog() {
   return (
-    <Layout page="blog" withLoader={true}>
-      <Section type={"list"}/>
+    <Layout page="blog">
+      <Section type={"list"} />
     </Layout>
   );
 }
